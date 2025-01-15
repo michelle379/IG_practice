8bbd3d1061e851fcbc6a99313e39f452ce6f4f2c
Michelle379 <michelle379123@gmail.com>
2025-01-15 15:44:00 +0800

    Initial Commit

---
+++ IG_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IG_practice
+//
+//  Created by 王宜婕 on 2025/1/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
